62391715e59b54c5637cb00c0d10c3364530448a
yonghanJu <65655825+yonghanJu@users.noreply.github.com>
2021-10-08 02:27:53 -0400

    Update BackupDialog.cpp

--- src/modules/partition/gui/BackupDialog.cpp
+++ src/modules/partition/gui/BackupDialog.cpp
@@ -70,4 +70,36 @@ void BackupDialog::on_backupButton_clicked()
 }
 
 
+void BackupDialog::on_pushButton_clicked(){
+    // leakness of capability
+    if(get_du_size(strCopy.c_str()) +1000 > get_df_size(strBackup.c_str())){
+    	QString er("not enough storage space, please reselect the file.");
+        ui->detailsLabel->setText(er);
+        er.clear();
+    }
+    // start backup
+    else{ 
+    	QString working("Working...");
+    	ui->detailsLabel->setText(working);
+    	working.clear();
+    	
+    	// done
+    	if(execBackup(strCopy, strBackup)){
+    	    QString success("The Backup is over!!");
+    	    ui->detailsLabel->setText(success);
+    	    success.clear();
+    	}
+    	// exception occurs!!
+    	else{
+    	    QString excp("Unexpected exception occured, please retry!!");
+    	    ui->detailsLabel->setText(excp);
+    	    excp.clear();
+    	}
+    }
+}
+
+void BackupDialog::on_pushButton_2_clicked(){
+    close();
+}
+
 
